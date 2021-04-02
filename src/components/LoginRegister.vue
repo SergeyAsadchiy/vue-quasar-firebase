6841b92f27220fd87054356f827362df9034e39a
SergeyAsadchiy <Sergey.Asadchiy@gmail.com>
2021-04-02 23:25:27 +0300

    auth page

---
+++ src/components/LoginRegister.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-form @submit="submitForm">
+    <q-input v-if="tab === 'register'" class="q-mb-md" v-model="formData.name" outlined label="Name"/>
+    <q-input class="q-mb-md" v-model="formData.email" outlined label="E-mail"/>
+    <q-input class="q-mb-md" v-model="formData.password" outlined label="Password"/>
+    <div class="row">
+      <q-space/>
+      <q-btn color="primary" type="submit" :label="tab"/>
+    </div>
+  </q-form>
+</template>
+
+<script>
+export default {
+  name: 'LoginRegister',
+  props: {
+    tab: null
+  },
+  data () {
+    return {
+      formData: {
+        name: '',
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    submitForm () {
+      if (this.tab === 'login') {
+        console.log('login the user')
+      } else {
+        console.log('register the user')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
